ecf9b57db5dd6a53b784ead0e83dbf3cdf8cbd8b
Vishal Gupta <65777217+vishal-CS24@users.noreply.github.com>
2023-05-16 01:15:04 +0530

    Add files via upload

---
+++ coffeetoppings.py
@@ -0,0 +1,14 @@
+# lex_auth_01269437590597632045
+import math
+
+
+def find_number_of_combination(number_of_flavours):
+    total_combination = math.pow(2, number_of_flavours)
+    # write your logic here
+
+    return total_combination
+
+
+# Provide different values for number_of_flavours and test your program
+number_of_combination = find_number_of_combination(6)
+print(number_of_combination)
